8a536f083d8d15c1a1cd988355b20b8e4af9bff3
Jiyong Jung <wjdwldyd1623@kaist.ac.kr>
2024-05-14 07:19:18 +0000

    add: improvements config

--- mmdet/models/detectors/dyn_retinanet.py
+++ mmdet/models/detectors/dyn_retinanet.py
@@ -141,7 +141,7 @@ class DynRetinaNet(SingleStageDetector):
         return x, y_early3, y_att, y_cnn, y_merge
     
     # Helper Functions
-    def get_dynamic_flops(self, data_loader, num_images=10):
+    def get_dynamic_flops(self, data_loader, num_images=100):
         # [CS470] 김남우, [CS470] 이찬규: [TODO]
         # 이건 뭐 별거는 아닌데, 우린 결국 flops별 accuarcy를 비교하는 것이니까
         # 이에 대한 내장 함수 하나 있어도 좋을 것 같다는 생각이 들어 일단 파놨습니다.
